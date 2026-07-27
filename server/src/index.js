b8c37ca3572438be099c24355d5fce849f95002b
Karan Singh Rawat <karansinghrwt75@gmail.com>
2026-07-27 10:37:48 +0530

    added morgan for http logs

--- server/src/index.js
+++ server/src/index.js
@@ -2,11 +2,11 @@ const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
 const connectDB = require('./config/db');
-
+const morgan = require('morgan');
 const app = express();
 app.use(express.json());
 app.use(cors());
-
+app.use(morgan('dev'));
 connectDB();
 
 app.get('/', (req, res) => {
